bff502e92befa384463629d8bbbbf3b0571436ee
JakMcCarthy <90810973+JakMcCarthy@users.noreply.github.com>
2022-08-19 16:45:32 -0400

    set variables

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var searchColumnEl = document.querySelector("#search-column");
+var citiesListContainerBtnEl = document.querySelector(".list-group-item");
+var dailyWeatherContainerEl = document.querySelector("#weather-container"); 
+
+
+// Find the city form
+var seachEventHandlerEl = document.querySelector("#city-search-form");
+var searchByCityEl = document.querySelector("#city-search");
+
