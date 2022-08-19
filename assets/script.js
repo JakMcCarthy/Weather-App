44ea4a1f98a3a82bf37123fe6318342278be5360
JakMcCarthy <90810973+JakMcCarthy@users.noreply.github.com>
2022-08-19 16:46:19 -0400

    call fetch

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,32 @@ var dailyWeatherContainerEl = document.querySelector("#weather-container");
 var seachEventHandlerEl = document.querySelector("#city-search-form");
 var searchByCityEl = document.querySelector("#city-search");
 
+
+function fetchSecondCall(searchByCity, latNum, lonNum, currentDateTime, currentDayIcon, currTempF, currentHumidity, currentMPS, mphWindSpeed) {
+
+    // Input API URL
+    var openWeatherApiFiveDayUrl =  "https://api.openweathermap.org/data/2.5/onecall?lat=" + latNum + "&lon=" + lonNum + "&appid=32a27c42260b02de3ba5e1466def4861&units=imperial"
+    
+    fetch(
+        openWeatherApiFiveDayUrl
+    )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (secondCallData) {
+        // Current Day
+        // UV info
+        var uvIndex = secondCallData.current.uvi;
+
+        var unix_timestamp = currentDateTime;
+        var date = new Date(unix_timestamp * 1000);
+        // date
+        var fullDayDaily = "(" + (date.getMonth() + 1) + "/" + date.getDate() + "/"  + date.getFullYear() + ")";
+                
+        // Populate current day data
+        populateCurrentDayHtml(searchByCity, fullDayDaily, currentDayIcon, currTempF, currentHumidity, currentMPS, mphWindSpeed, uvIndex);
+
+        // Populate 5 day forcast
+        populate5DayForecast(secondCallData);
+    });
+};
